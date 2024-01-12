415757b4380308338b1f54f520c88b3d883c4a88
codechefsong <133816663+codechefsong@users.noreply.github.com>
2024-01-12 16:52:50 -0500

    change the home page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,6 +1,6 @@
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
@@ -11,48 +11,39 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Quantum Space Gems</span>
           </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/pages/index.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <Image className="ml-8" alt="Game" width={500} height={350} src="/game.png" />
+          <p className="text-center text-lg">Embark on a journey to planets and collect precious gems</p>
+          <div className="flex justify-center mb-2">
+            <Link
+              href="/board"
+              passHref
+              className=" py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-400 disabled:opacity-50"
+            >
+              Play
+            </Link>
+          </div>
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+          <div className="text-center">
+            <h2 className="mt-3 text-4xl">Gameplay</h2>
+          </div>
+          <div className="flex justify-center">
+            <ul className="list-disc" style={{ width: "600px" }}>
+              <li>Player have to buy and mint Troop NFT to play</li>
+              <li>Player can deploy the Troop on to the planet</li>
+              <li>Each troop start with 50 oxygen points</li>
+              <li>Moving troop costs 1 oxygen points</li>
+              <li>You can capture the gem field and mine for Gem as ERC20 tokens</li>
+            </ul>
+          </div>
+          <p className="text-3xl text-center">Requirements</p>
+          <div className="flex justify-center">
+            <ul className="list-disc" style={{ width: "600px" }}>
+              <li>To play, you must mint a Troop NFT to play</li>
+            </ul>
           </div>
         </div>
       </div>
